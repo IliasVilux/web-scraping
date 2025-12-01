ef085e8f4981b42e6a7147933846a4feeedeebb9
IliasVilux <iliasotsman@gmail.com>
2025-12-01 01:20:42 +0100

    refactor: Update "index.js" structure for modularity

--- index.js
+++ index.js
@@ -1,38 +1,13 @@
 import puppeteer from "puppeteer";
 import fs from "fs";
+import { scrapeBoycottIsrael } from "./scrapers/boycottIsrael.js";
 
 const browser = await puppeteer.launch({
   headless: "new",
   defaultViewport: null,
 });
-const page = await browser.newPage();
-await page.goto("https://boycott-israel.org/boycott.html", {
-  waitUntil: "domcontentloaded",
-});
 
-const data = await page.$$eval(".boycott-category", (categories) => {
-  const result = {};
+const boycottIsrael = await scrapeBoycottIsrael(browser);
+fs.writeFileSync("data/boycott-israel.json", JSON.stringify(boycottIsrael, null, 2));
 
-  const parseBrand = (brand) => ({
-    name: brand.querySelector("h4")?.childNodes[0]?.textContent.trim() ?? null,
-    impact: brand.querySelector("span")?.innerText.trim().toLowerCase() ?? null,
-    description: brand.querySelector("h4 + p")?.innerText.trim() ?? null,
-    subbrands: Array.from(brand.querySelectorAll(".subbrands > div"), (sub) =>
-      sub.textContent.trim()
-    ),
-    country: brand.querySelector(".country")?.innerText.trim() ?? null,
-    alternative: brand.querySelector(".alternative")?.innerText.trim() ?? null,
-    source: brand.querySelector("a")?.href ?? null,
-  });
-
-  for (const category of categories) {
-    const categoryName = category.querySelector("h3")?.innerText.trim();
-
-    const brands = Array.from(category.querySelectorAll("li"), parseBrand);
-
-    result[categoryName] = brands;
-  }
-  return result;
-});
-fs.writeFileSync("data.json", JSON.stringify(data, null, 2));
 await browser.close();
