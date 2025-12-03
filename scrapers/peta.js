dc31947d4e3172849392136555c1e9d11efe1131
IliasVilux <iliasotsman@gmail.com>
2025-12-03 17:57:32 +0100

    feat: :sparkles: Load data from every page

--- scrapers/peta.js
+++ scrapers/peta.js
@@ -1,20 +1,34 @@
 export async function scrapePeta(browser) {
-  const page = await browser.newPage();
-  await page.goto("https://crueltyfree.peta.org/companies-do-test/", {
-    waitUntil: "networkidle2",
-  });
+  const allBrands = [];
+  let pageNum = 1;
+  let hasNextPage = true;
 
-  const brands = await page.$$eval("ul.search-results li", (nodes) => {
-    return nodes.map((node) => ({
-        name: node.querySelector("a")?.innerText.trim() ?? null,
-        link: node.querySelector("a")?.href ?? null,
-    }));
-  });
+  while (hasNextPage) {
+    const page = await browser.newPage();
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    );
+    await page.goto(`https://crueltyfree.peta.org/companies-do-test/page/${pageNum}/`, {
+      waitUntil: "networkidle2",
+    });
+    
+      const brands = await page.$$eval("ul.search-results li", (nodes) => {
+        return nodes.map((node) => ({
+            name: node.querySelector("a")?.innerText.trim() ?? null,
+            link: node.querySelector("a")?.href ?? null,
+        }));
+      });
 
-  await page.close();
+      hasNextPage = await page.$eval("ul.pagination li:last-child a.next", () => true).catch(() => false)
+
+      await page.close();
+      allBrands.push(...brands);
+      pageNum++;
+  }
+
   const results = {};
 
-  for (const brand of brands) {
+  for (const brand of allBrands) {
     await sleep();
 
     const parsed = await parseBrandPage(browser, brand);
